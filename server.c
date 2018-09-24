11b14eb6eb3efad1ac074a02b3a4d59024de25f8
Tim <garif-tim@yandex.ru>
2018-09-24 19:18:58 +0300

    add more mimetypes

--- server.c
+++ server.c
@@ -54,7 +54,7 @@ int main(int argc, char const *argv[])
         }
 
         recv(sock, buf, sizeof(buf), 0);
-        printf("\n====\n%s\n======\n", buf);
+        // printf("\n====\n%s\n======\n", buf);
 
         sscanf(buf, "%s %s %s\n", request.method, request.uri, request.version);
 
@@ -64,35 +64,51 @@ int main(int argc, char const *argv[])
 
         if(strstr(request.uri, ".html")) {
             strcpy(request.mimeType, "text/html");
-        }
+        } else
+        if(strstr(request.uri, ".css")) {
+            strcpy(request.mimeType, "text/css");
+        } else
+        if(strstr(request.uri, ".js")) {
+            strcpy(request.mimeType, "application/javascript");
+        } else
         if(strstr(request.uri, ".jpg") || strstr(request.uri, ".jpeg")) {
             strcpy(request.mimeType, "image/jpeg");
-        }
+        } else
         if(strstr(request.uri, ".png")) {
             strcpy(request.mimeType, "image/png");
+        } else
+        if(strstr(request.uri, ".gif")) {
+            strcpy(request.mimeType, "image/gif");
+        } else
+        if(strstr(request.uri, ".swf")) {
+            strcpy(request.mimeType, "application/x-shockwave-flash");
         }
 
         printf("\n%s\n%s\n%s\n", request.method, request.uri, request.version);
 
-        fs = fopen(request.uri+1, "r");
-        if (fs == NULL) {
-            strcpy(buf, "HTTP/1.1 404 NOT FOUND\n\n\0");
-            strcpy(buf + strlen(buf), "<h1 align='center'>404 NOT FOUND<h1>\0");
-            send(sock, buf, strlen(buf), 0);
-        } else {
-            strcpy(buf, "HTTP/1.1 200 OK\nContent-Type: ");
-            strcpy(buf+strlen(buf), request.mimeType);
-            strcpy(buf+strlen(buf), "\n\n");
-            send(sock, buf, strlen(buf), 0);
-            printf("%d  %s", strlen(buf), buf);
+        if (!strstr(request.uri, "..")) {
+            fs = fopen(request.uri+1, "r");
+            if (fs == NULL) {
+                strcpy(buf, "HTTP/1.1 404 NOT FOUND\n\n\0");
+                strcpy(buf + strlen(buf), "<h1 align='center'>404 NOT FOUND<h1>\0");
+                send(sock, buf, strlen(buf), 0);
+            } else {
+                strcpy(buf, "HTTP/1.1 200 OK\nContent-Type: ");
+                strcpy(buf+strlen(buf), request.mimeType);
+                strcpy(buf+strlen(buf), "\n\n");
+                send(sock, buf, strlen(buf), 0);
 
-            int len;
-            while (len = fread(buf, sizeof(char), 1024, fs)) {
-                send(sock, buf, len, 0);
+                int len;
+                while (len = fread(buf, sizeof(char), 1024, fs)) {
+                    send(sock, buf, len, 0);
+                }
+                fclose(fs);
             }
-            fclose(fs);
+        } else {
+            strcpy(buf, "HTTP/1.1 403 FORBIDDEN\n\n\0");
+            strcpy(buf + strlen(buf), "<h1 align='center'>403 FORBIDDEN<h1>\0");
+            send(sock, buf, strlen(buf), 0);            
         }
-
         close(sock);
     }
     
